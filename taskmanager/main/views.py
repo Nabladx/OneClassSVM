15a24080ed19c1c155708eb93764d829386b8ed3
akirilichev <akirilichev@ptsecurity.com>
2022-04-07 10:08:51 +0700

    Initial commit

---
+++ taskmanager/main/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Home page '
+                                                        'of this cite',
+                                               'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Form was wrong'
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+# Create your views here.
